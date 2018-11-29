06ee054edb44eff1f1fb454dcfe86138939875f4
lovecross <lovecrossyoutt@hotmail.com>
2018-11-29 23:29:35 +0800

    xx

--- .umirc.js
+++ .umirc.js
@@ -18,7 +18,6 @@ export default {
   theme: './config/theme.config.js',
   proxy: {
     "/api": {
-      // target: "http://123.57.161.212:9939/xitenggamejar/",
       // "target": "http://192.168.1.83:9939/xitenggamejar/",
       target: "https://www.xiteng.com/xitenggamejar/",
       changeOrigin: true,
