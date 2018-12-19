4ec0723365c4fb4a880bba5d7d6dff6f34b3e7c4
lovecross <lovecrossyoutt@hotmail.com>
2018-12-19 22:11:29 +0800

    xx

--- src/pages/withdraw/page.js
+++ src/pages/withdraw/page.js
@@ -4,6 +4,7 @@
 import React from 'react';
 import { routerRedux } from 'dva/router';
 import {connect} from 'dva';
+import DocumentTitle from 'react-document-title';
 
 import {List, InputItem, WhiteSpace, Button, WingBlank, ImagePicker, Icon} from 'antd-mobile';
 import {createForm} from 'rc-form';
@@ -16,30 +17,31 @@ const confirmClick = ()=>{
 }
 
 function WithDraw(props) {
-  return <div>
-    <div className={styles.header}>
-      <img src={money_icon} alt="" className={styles.money_icon}/>
-      <div className={styles.remain_amount}>
-        余额
+  return <DocumentTitle title='提现'>
+    <div>
+      <div className={styles.header}>
+        <img src={money_icon} alt="" className={styles.money_icon}/>
+        <div className={styles.remain_amount}>
+          余额
+        </div>
+        <div className={styles.amount}>
+          2553.00
+        </div>
       </div>
-      <div className={styles.amount}>
-        2553.00
+      <div className={styles.footer_btn}>
+        <Button
+          type="primary"
+          onClick={()=>{
+            props.dispatch(routerRedux.push('/withdraw/withdraw'))
+          }}>提现</Button>
+      </div>
+      <div className={styles.record}>
+        <div className={styles.record_text}>
+          提现记录
+        </div>
       </div>
     </div>
-    <div className={styles.footer_btn}>
-      <Button
-        type="primary"
-        onClick={()=>{
-          props.dispatch(routerRedux.push('/withdraw/withdraw'))
-        }}>提现</Button>
-    </div>
-
-    <div className={styles.record}>
-      <div className={styles.record_text}>
-        提现记录
-      </div>
-    </div>
-  </div>
+  </DocumentTitle>
 }
 
 export default connect()(WithDraw)
